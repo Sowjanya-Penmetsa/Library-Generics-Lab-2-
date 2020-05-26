dc081c48e13d24700e48e1ac10c5d2d2277a2d87
unknown <penmetsa.sowjanya30@gmail.com>
2020-05-26 14:21:19 +0200

    first commit

---
+++ Library/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Library
+{
+    public class Book:IComparable<Book>
+    {
+        
+        public Book(string title,int year,params string[]authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+            
+        }
+        public string Title
+        {
+            get;set;
+
+        }
+        public int Year
+        {
+            get;set;
+        }
+        public IReadOnlyList <String> Authors
+        {
+            get;set;
+
+        }
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if(result==0)
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+        public override string ToString()
+        { 
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
---
+++ Library/Library.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+
+    class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+        //Constructor
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books);
+            
+        }
+        // Implementing GetEnumerator Method of IEnumerable Interface
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        //Internal class called LibraryIterator For holding different lists
+        class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+            //Constructor of LibraryIterator
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+           // Implementation of methods of IEnumerator Interface
+            public void Dispose()
+            {
+
+            }
+            public bool MoveNext() => ++this.currentIndex < this.books.Count;
+            public void Reset() => this.currentIndex = -1;
+            public Book Current => this.books[this.currentIndex];
+            object IEnumerator.Current => this.Current;
+        }
+
+    }
+}
